490424ca042cb4a516d0cb0493f9e1344c89d989
Driss-001 <drissmann001@gmail.com>
2022-11-11 13:15:18 -0500

    moved decision_master from IMG

--- Imgs/Decision_master.py
+++
@@ -1,8 +0,0 @@
-import math  as mt 
-import serial , re ,time,sys ,threading ,serial.tools.list_ports
-
-
-
-#decision  dictionnaries
-lamp_modes = {"off":0,"on":1}
-road_states = {"None":0,"pedestrian":1,"Car":2}
