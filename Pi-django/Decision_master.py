b5e0249a0391966efd188c17f76eaadec12a7b09
DrissOttw <iamad056@uottawa.ca>
2022-11-18 15:37:56 -0500

    created the class decision_master

--- Pi-django/Decision_master.py
+++ Pi-django/Decision_master.py
@@ -5,17 +5,35 @@ import time
 
 
 
-#decision  lists for quick access
-lamp_modes = ["off","on"]
-road_states = ['None',"pedestrian","Car"]
+
+class Decision_master:
+
+    def __init__(self,com1,com2):
+        
+        self.load_sensor = com1
+        self.Ir_sensor = com2
+        #Constants
+        #15cm between IR sensors
+        self.delta_d = 15e-2
+        #decision  lists for quick access
+        self.lamp_modes = ["off","on"]
+        road_states = ['None',"pedestrian","Car"]
+        self.current_lamp_mode = self.lamp_modes[0]
+        self.current_road_state = self.road_states[0]
 
 
-#mock random signal generator
-rand_lamp_mode = rd.randint(0,len(lamp_modes)-1)
-rand_road_state = rd.randint(0,len(road_states)-1)
 
-current_lamp_mode = lamp_modes[rand_lamp_mode]
-current_road_state = road_states[rand_road_state]
+
+    def __decision_randomizer(self):
+        #mock random signal generator
+        rand_lamp_mode = rd.randint(0,len(self.lamp_modes)-1)
+        rand_road_state = rd.randint(0,len(self.road_states)-1)
+        self.current_lamp_mode = self.lamp_modes[rand_lamp_mode]
+        self.current_road_state = self.road_states[rand_road_state]
+
+
+    #def __apply_decision(self):
+
 
 
 
