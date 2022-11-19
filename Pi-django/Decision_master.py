5e15a919abf140f0ccb348f8d59d12c70d60b5b2
Driss-001 <drissmann001@gmail.com>
2022-11-19 05:14:13 -0500

    implemented threading, modified errors

--- Pi-django/Decision_master.py
+++ Pi-django/Decision_master.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 import math  as mt 
 import random as rd
 import serial , re ,time,sys ,threading ,serial.tools.list_ports
@@ -7,21 +8,21 @@ import time
 ports = list(serial.tools.list_ports.comports())
 
 for p in ports:
-    print(p)
-    if "Silicon Labs" in p.description:
-        com  =p.name
+    if "ttyAMA0" in p.description:
+        com1  =p.name
 
 class Decision_master:
 
-    def __init__(self,com1):
+    def __init__(self,com):
         self.baudrate = 76800
-        self.sensors = com1
+        self.com = com
         #Constants
         #15cm between IR sensors
+        self.reading = True
         self.delta_d = 15e-2
         #decision  lists for quick access
         self.lamp_modes = ["off","on"]
-        road_states = ['None',"pedestrian","Car"]
+        self.road_states = ['None',"pedestrian","Car"]
         self.current_lamp_mode = self.lamp_modes[0]
         self.current_road_state = self.road_states[0]
 
@@ -36,11 +37,28 @@ class Decision_master:
         self.current_road_state = self.road_states[rand_road_state]
 
     
-    #def __serial_measurement(self):
+    def __serial_measurement(self):
+        if self.com != None:
+            ser = serial.serial(self.com)
+            ser.baudrate = self.baudrate
+            while self.reading:
+                self.raw_data = ser.readline().decode(("ascii"))
+                print(self.raw_data)
+                time.sleep(1e-3)
 
+    def Reading_thread(self):
+        self.thread1 = threading.thread(target = self.__serial_measurement)
+        self.thread1.daemon = True
+        self.thread.start()
+        time.sleep(5)
+        self.reading = False
+        self.thread1.join()
+        
 
     #def __apply_decision(self):
 
-
+if __name__ == "__main__":
+    D1 = Decision_master(com1)
+    D1.Reading_thread()
 
 
